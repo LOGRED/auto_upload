e61722c0688a5bc5f180358ff4392b7c4e83c849
LOGRED <log_sh@naver.com>
2025-07-21 22:46:55 +0900

    update

--- auto-push.js
+++ auto-push.js
@@ -1,7 +1,8 @@
 import chokidar from 'chokidar';
 import {sep} from 'path';
 import axios from 'axios';
-
+import fs from 'fs';
+import FormData from 'form-data';
 
 const detectFolder = 'C:/Users/sihan/OneDrive/문서/마음렌즈/Maumlenz/database/vision';
 
@@ -14,11 +15,13 @@ watcher.on('change', async (path) => {
     if (path.endsWith('.xlsm')) {
         console.log(path.split(sep).pop());
 
-        const form = new FormData();
-        form.append('file', path);
-
-        const response = await axios.post('https://dev.vision21tech.com/api/upload', form)
-
-        console.log(response);
+        try {
+            const form = new FormData();
+            form.append('file', fs.createReadStream(path));
+            const response = await axios.post('https://dev.vision21tech.com/api/upload', form)
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+        }
     }
 });
