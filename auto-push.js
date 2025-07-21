42216113163134202aea188d31d39631da73fa95
LOGRED <log_sh@naver.com>
2025-07-21 22:50:26 +0900

    update

--- auto-push.js
+++ auto-push.js
@@ -6,6 +6,8 @@ import FormData from 'form-data';
 
 const detectFolder = 'C:/Users/sihan/OneDrive/문서/마음렌즈/Maumlenz/database/vision';
 
+console.log("에이전트가 켜졌습니다")
+
 const watcher = chokidar.watch(detectFolder, {
     ignored: ['.git/**', 'node_modules/**'],
     ignoreInitial: true
@@ -19,7 +21,7 @@ watcher.on('change', async (path) => {
             const form = new FormData();
             form.append('file', fs.createReadStream(path));
             const response = await axios.post('https://dev.vision21tech.com/api/upload', form)
-            console.log(response);
+            console.log('서버로 성공적으로 데이터를 보냈습니다');
         } catch (err) {
             console.log(err);
         }
